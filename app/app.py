2814f331875127521fc0dc1c4b29808fb1b6f6e3
Patrick Gendotti <patrick.gendotti@gmail.com>
2020-10-28 11:53:40 -0700

    feat: Add model prediction capability to app - can now output results

--- app/app.py
+++ app/app.py
@@ -33,24 +33,33 @@ def predict():
     # Get the data from the POST request.
     if request.method == "POST":
         #data = request.get_json(force=True)
-        checker = [a for a in request.form.keys()]
-        print(checker)
+        # checker = [a for a in request.form.keys()]
+        # print(checker)
         data_cols = ['EMI', 'loan_amount', 'maximum_amount_sanctioned', 'age', 'rate_of_interest',
-                     'past_due_30', 'number_of_loans', 'maximum_sanctioned', 'past_due_90', 'tenure', 
+                     'past_due_30', 'max_mob', 'past_due_60', 'number_of_loans', 'maximum_sanctioned', 'past_due_90', 'tenure',
                      'times_bounced']
         data = [request.form[a] for a in data_cols]
-        print(data)
-        data = X_scaler.transform(data)
-        
-        # Make prediction using model loaded from disk as per the data.
-        prediction = model.predict(data)
+        # print(data)
+        try:
+            data = X_scaler.transform(data)
+            
+            # Make prediction using model loaded from disk as per the data.
+            prediction = model.predict(data)[0]
 
-        print("Data", prediction)
+            print("Data", prediction)
 
+        except ValueError:
+            prediction = 2
+
+        outcomes = {
+            0: "Default risk sufficiently low.",
+            1: "Default risk high - personal loan offer prohibited.",
+            2: "Form error - validate your inputs on the last page."
+        }
         # Take the first value of prediction
         # output = prediction[0]
 
-        return render_template("results.html", output=checker)#, exp=data)
+        return render_template("results.html", output=outcomes[prediction])#, exp=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
